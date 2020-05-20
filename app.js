5f2bdf62117999db8b3091ccaa18fac6e9d88bbd
Prithviraj Kadiyala <prithvirajkadiyala@gmail.com>
2020-05-20 14:55:11 -0500

    Never type

--- app.js
+++ app.js
@@ -1,18 +1,11 @@
-function add(n1, n2) {
-    return n1 + n2;
+var userInput;
+var userName;
+userInput = 5;
+userInput = 'Max';
+if (typeof userInput === 'string') {
+    userName = userInput;
 }
-function printResult(num) {
-    console.log('Result ' + num);
+function generateError(message, code) {
+    throw { message: message, errorCode: code };
 }
-function addAndHandle(n1, n2, cb) {
-    var result = n1 + n2;
-    cb(result);
-}
-printResult(34);
-var combineValues;
-combineValues = add;
-console.log(combineValues(8, 8));
-addAndHandle(10, 20, function (result) {
-    console.log(result);
-    return result;
-});
+generateError('A Error Occured', 500);
--- app.ts
+++ app.ts
@@ -6,3 +6,9 @@ userInput = 'Max';
 if (typeof userInput === 'string'){
     userName = userInput;
 }
+
+function generateError(message: string, code: number): never {
+    throw { message: message, errorCode: code };
+}
+
+generateError('An Error Occured', 500);
