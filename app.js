312e1ccb2c97af8665c0696f7b53353bc4a2d147
Prithviraj Kadiyala <prithvirajkadiyala@gmail.com>
2020-05-20 14:03:44 -0500

    Enum

--- app.js
+++ app.js
@@ -1,13 +1,40 @@
-function add(n1, n2, showResult) {
-    if (showResult) {
-        console.log(n1 + n2);
-    }
-    else {
-        return n1 + n2;
-    }
+/*const person: {
+    name: string;
+    age: number;
+    hobbies: string[];
+    role: [number, string];
+} = {
+    name: 'Prithviraj',
+    age: 30,
+    hobbies: ['Sports', 'Cooking'],
+    role: [2, 'author']
+};*/
+var ADMIN = 0;
+var READ_ONLY = 1;
+var AUTHOR = 2;
+var Role;
+(function (Role) {
+    Role[Role["ADMIN"] = 0] = "ADMIN";
+    Role[Role["READ_ONLY"] = 1] = "READ_ONLY";
+    Role[Role["AUTHOR"] = 2] = "AUTHOR";
+})(Role || (Role = {}));
+;
+var person = {
+    name: 'Prithviraj',
+    age: 30,
+    hobbies: ['Sports', 'Cooking'],
+    role: Role.ADMIN
+};
+//person.role.push('admin');
+//person.role[1] = 10;
+//person.role = [0, 'admin', 'user']; 
+var favouriteActivities;
+favouriteActivities = ['Sports'];
+console.log(person.name);
+for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
+    var hobby = _a[_i];
+    console.log(hobby.toUpperCase());
 }
-var number1 = '5';
-var number2 = 10;
-var printResult = true;
-var result = add(+number1, +number2, printResult);
-console.log(result);
+if (person.role == ADMIN) {
+    console.log('This is the Admin...');
+}
--- app.ts
+++ app.ts
@@ -1,17 +1,41 @@
-function add(n1: number, n2: number, showResult: boolean) {
-    if(showResult){
-        console.log(n1 + n2);
-    }
-    else{
-        return n1+ n2;
-    }
-    
+/*const person: {
+    name: string;
+    age: number;
+    hobbies: string[];
+    role: [number, string];
+} = {
+    name: 'Prithviraj',
+    age: 30,
+    hobbies: ['Sports', 'Cooking'],
+    role: [2, 'author']
+};*/
+const ADMIN =0;
+const READ_ONLY = 1;
+const AUTHOR = 2;
+
+enum Role {ADMIN, READ_ONLY, AUTHOR};
+
+const person = {
+    name: 'Prithviraj',
+    age: 30,
+    hobbies: ['Sports', 'Cooking'],
+    role: Role.ADMIN
+};
+//person.role.push('admin');
+//person.role[1] = 10;
+
+//person.role = [0, 'admin', 'user']; 
+
+
+let favouriteActivities: string[];
+favouriteActivities = ['Sports'];
+
+console.log(person.name);
+
+for(const hobby of person.hobbies){
+    console.log(hobby.toUpperCase());
 }
 
-const number1 = '5';
-const number2 = 10;
-
-const printResult = true;
-
-const result = add(+number1, +number2, printResult);
-console.log(result);
+if(person.role == ADMIN){
+    console.log('This is the Admin...')
+}
