b89f9dcffa0326c1780f470cb60230a4ab4e47a2
Prithviraj Kadiyala <prithvirajkadiyala@gmail.com>
2020-05-20 13:33:02 -0500

    Type Assignments

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
-console.log('Your code goes here!');
+function add(n1, n2, showResult) {
+    if (showResult) {
+        console.log(n1 + n2);
+    }
+    else {
+        return n1 + n2;
+    }
+}
+var number1 = '5';
+var number2 = 10;
+var printResult = true;
+var result = add(+number1, +number2, printResult);
+console.log(result);
