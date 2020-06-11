915ec5c91e952a8b8515f32af2e42da90d70e9da
Prithviraj Kadiyala <prithvirajkadiyala@gmail.com>
2020-06-11 00:59:59 -0500

    Generic vs Utility types

--- src/app.ts
+++ src/app.ts
@@ -33,3 +33,69 @@ function merge<T extends object, U extends object>(objA: T, objB: U) {
   }
   
   console.log(countAndDescribe(['Sports', 'Cooking']));
+  
+  function extractAndConvert<T extends object, U extends keyof T>(
+    obj: T,
+    key: U
+  ) {
+    return 'Value: ' + obj[key];
+  }
+  
+  extractAndConvert({ name: 'Max' }, 'name');
+  
+  class DataStorage<T extends string | number | boolean> {
+    private data: T[] = [];
+  
+    addItem(item: T) {
+      this.data.push(item);
+    }
+  
+    removeItem(item: T) {
+      if (this.data.indexOf(item) === -1) {
+        return;
+      }
+      this.data.splice(this.data.indexOf(item), 1); // -1
+    }
+  
+    getItems() {
+      return [...this.data];
+    }
+  }
+  
+  const textStorage = new DataStorage<string>();
+  textStorage.addItem('Max');
+  textStorage.addItem('Manu');
+  textStorage.removeItem('Max');
+  console.log(textStorage.getItems());
+  
+  const numberStorage = new DataStorage<number>();
+  
+  // const objStorage = new DataStorage<object>();
+  // const maxObj = {name: 'Max'};
+  // objStorage.addItem(maxObj);
+  // objStorage.addItem({name: 'Manu'});
+  // // ...
+  // objStorage.removeItem(maxObj);
+  // console.log(objStorage.getItems());
+  
+  interface CourseGoal {
+    title: string;
+    description: string;
+    completeUntil: Date;
+  }
+  
+  function createCourseGoal(
+    title: string,
+    description: string,
+    date: Date
+  ): CourseGoal {
+    let courseGoal: Partial<CourseGoal> = {};
+    courseGoal.title = title;
+    courseGoal.description = description;
+    courseGoal.completeUntil = date;
+    return courseGoal as CourseGoal;
+  }
+  
+  const names: Readonly<string[]> = ['Max', 'Anna'];
+  // names.push('Manu');
+  // names.pop();
