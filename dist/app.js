670587cffe51b8fe63d7e3a9502f1c1a2330893e
Prithviraj Kadiyala <prithvirajkadiyala@gmail.com>
2020-06-11 01:42:07 -0500

    Decorator Return types

--- dist/app.js
+++ dist/app.js
@@ -5,20 +5,30 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
     else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
+var __param = (this && this.__param) || function (paramIndex, decorator) {
+    return function (target, key) { decorator(target, key, paramIndex); }
+};
 function Logger(logString) {
+    console.log('LOGGER FACTORY');
     return function (constructor) {
         console.log(logString);
         console.log(constructor);
     };
 }
 function WithTemplate(template, hookId) {
-    return function (constructor) {
-        const hookEl = document.getElementById(hookId);
-        const p = new constructor();
-        if (hookEl) {
-            hookEl.innerHTML = template;
-            hookEl.querySelector('h1').textContent = p.name;
-        }
+    console.log('TEMPLATE FACTORY');
+    return function (originalConstructor) {
+        return class extends originalConstructor {
+            constructor(..._) {
+                super();
+                console.log('Rendering template');
+                const hookEl = document.getElementById(hookId);
+                if (hookEl) {
+                    hookEl.innerHTML = template;
+                    hookEl.querySelector('h1').textContent = this.name;
+                }
+            }
+        };
     };
 }
 // @Logger('LOGGING - PERSON')
@@ -30,9 +40,65 @@ let Person = /** @class */ (() => {
         }
     };
     Person = __decorate([
+        Logger('LOGGING'),
         WithTemplate('<h1>My Person Object</h1>', 'app')
     ], Person);
     return Person;
 })();
 const pers = new Person();
 console.log(pers);
+// ---
+function Log(target, propertyName) {
+    console.log('Property decorator!');
+    console.log(target, propertyName);
+}
+function Log2(target, name, descriptor) {
+    console.log('Accessor decorator!');
+    console.log(target);
+    console.log(name);
+    console.log(descriptor);
+}
+function Log3(target, name, descriptor) {
+    console.log('Method decorator!');
+    console.log(target);
+    console.log(name);
+    console.log(descriptor);
+}
+function Log4(target, name, position) {
+    console.log('Parameter decorator!');
+    console.log(target);
+    console.log(name);
+    console.log(position);
+}
+let Product = /** @class */ (() => {
+    class Product {
+        constructor(t, p) {
+            this.title = t;
+            this._price = p;
+        }
+        set price(val) {
+            if (val > 0) {
+                this._price = val;
+            }
+            else {
+                throw new Error('Invalid price - should be positive!');
+            }
+        }
+        getPriceWithTax(tax) {
+            return this._price * (1 + tax);
+        }
+    }
+    __decorate([
+        Log
+    ], Product.prototype, "title", void 0);
+    __decorate([
+        Log2
+    ], Product.prototype, "price", null);
+    __decorate([
+        Log3,
+        __param(0, Log4)
+    ], Product.prototype, "getPriceWithTax", null);
+    return Product;
+})();
+const p1 = new Product('Book', 19);
+const p2 = new Product('Book 2', 29);
