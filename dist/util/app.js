a93041858706e03a8f55025524c586ee93f95952
Prithviraj Kadiyala <prithvirajkadiyala@gmail.com>
2020-07-05 20:24:25 -0500

    Solved the errors

--- dist/util/app.js
+++ dist/util/app.js
@@ -1,7 +1,7 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-const project_input_js_1 = require("./components/project-input.js");
-const project_list_js_1 = require("./components/project-list.js");
+const project_input_js_1 = require("../components/project-input.js");
+const project_list_js_1 = require("../components/project-list.js");
 new project_input_js_1.ProjectInput();
 new project_list_js_1.ProjectList('active');
 new project_list_js_1.ProjectList('finished');
--- src/util/app.ts
+++ src/util/app.ts
@@ -1,5 +1,5 @@
-import { ProjectInput } from './components/project-input.js';
-import { ProjectList } from './components/project-list.js';
+import { ProjectInput } from '../components/project-input.js';
+import { ProjectList } from '../components/project-list.js';
 
 new ProjectInput();
 new ProjectList('active');
